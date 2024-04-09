a34f52061f3c9325d7ab3bcf4254e21558068258
jrazec <johnrazeagno@gmail.com>
2024-04-09 14:07:42 +0800

    Fix folder management

--- routes/admin.js
+++ routes/admin.js
@@ -2,28 +2,30 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+// Used these modules to use the views and folder directory
+router.use(express.static("views")); // For static images, css and js transitions
+router.use(express.static("public"));// for dynamic pages etc.
 
-router.use(express.static("admin"));
 
 // Admin Login Page
 router.get("/login",(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','admin','html','admin-login.html'))
+    res.sendFile(path.join(__dirname,'..','views','admin','admin-login.html'))
 });
 
 router.get("/home",(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','admin','html','home.html'))
+    res.sendFile(path.join(__dirname,'..','views','admin','home.html'))
 });
 
 router.get("/setup/quest",(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','admin','html','quest.html'))
+    res.sendFile(path.join(__dirname,'..','views','admin','quest.html'))
 });
 
 router.get("/setup/map",(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','admin','html','map.html'))
+    res.sendFile(path.join(__dirname,'..','views','admin','map.html'))
 });
 
 router.get("/setup/student",(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','admin','html','student.html'))
+    res.sendFile(path.join(__dirname,'..','views','admin','student.html'))
 });
 
 
