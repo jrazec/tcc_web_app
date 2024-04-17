ee11ad45d5c36125fe4726b8fd4bac11ed5a82ac
jrazec <johnrazeagno@gmail.com>
2024-04-17 21:44:43 +0800

    Add putController for edit, empty route {obj}

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,15 +4,18 @@ const path = require("path");
 const bodyParser = require('body-parser'); // Parsing the req 
 const methodOverride = require('method-override'); // For overriding the _method? PUT
 const connectMysql = require('../config/db');
-const getController = require("../controllers/getController");
 
+
+
+const getController = require("../controllers/getController");
+const putController = require("../controllers/putController")
 
 router.use(bodyParser.json())
 router.use(methodOverride('_method='));// Middle ware to just send post request in frontend
 router.use(express.urlencoded({ extended: true }));
 router.use(express.static("public"));// For static images, css and js transitions
 
-console.log(connectMysql)
+
 
 
 
@@ -23,7 +26,7 @@ router.get("/login",(req,res)=>{
 
 // Home Page - Statistics Page
 router.get("/home",(req,res)=>{
-    res.render('admin/index', { content : "home" });
+    res.render('admin/index', { content : "home", route : "" });
 });
 
 //-----------------------Quest Setting Up Page-----------------------
@@ -37,46 +40,7 @@ router
 router
     .route("/setup/npc/edit/:id")
     .get(getController.getSingleNpc)
-    .post((req,res)=>{//UPDATE
-        console.log("Put Request")
-        const npc_id = String(req.body["npc-id"]); 
-        const npc_name = String(req.body["npc-name"]);
-        const npc_description = String(req.body["npc-desc"]);
-        const npc_image = String(req.body["npc-image"]);
-        const coordinate = String(req.body["coordinates"]);
-
-        let updateValuesT1 = `UPDATE npcs 
-                              SET npc_name=\"${npc_name}\",npc_description=\"${npc_description}\",npc_image=\"${npc_image}\" 
-                              WHERE npc_id=${npc_id};`;
-        let updateValuesT2 = `UPDATE designation 
-                              SET coordinate=${coordinate} 
-                              WHERE npc_id=${npc_id};`;
-        con.connect(err=>{
-            if(err) throw err;
-
-            // Transaction 1 for NPC table
-            con.query((updateValuesT1),(err,result,field)=>{
-                if(err) {
-                    console.log("Failed T1 Update");
-                } // throw an error message
-                else {
-                    console.log("Success T1 Update");
-                }
-            });
-            // Transaction 2 for Designation Table
-            con.query((updateValuesT2),(err,result,field)=>{
-                if(err) {
-                    console.log("Failed T2 Update");
-                } // throw an error message
-                else {
-                    console.log("Success T2 Update");
-                }
-            });
-        })
-        console.log(npc_id,npc_name,npc_description,npc_image,coordinate)
-        res.redirect("/admin/setup/npc/");
-
-    });
+    .post(putController.editSingleNpc);
 
 router
     .route("/setup/classroom")
@@ -102,13 +66,13 @@ router
 
 //-----------------------Map Setting up Page-----------------------
 router.get("/setup/map",(req,res)=>{
-    res.render('admin/index', { content : "map"});
+    res.render('admin/index', { content : "map",  route : "" });
 });
 
 
 // Student Setting up Page
 router.get("/setup/student",(req,res)=>{
-    res.render('admin/index', { content : "student"});
+    res.render('admin/index', { content : "student", route : "" });
 });
 
 
