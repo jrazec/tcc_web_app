58e2571c9eecab4ea64b5124fd3855fa28924beb
jrazec <johnrazeagno@gmail.com>
2024-04-18 19:58:21 +0800

    Add checkHighestNpcId,DesigId,addNpc,deleteNpc

--- server/model/Npc.js
+++ server/model/Npc.js
@@ -8,6 +8,7 @@ class npcTable {
         this.npc_image = npc_image
     }
 
+    // -------- Update Transaction -----------
     static editSingle(npcId,npcName,npcDes,npcImg,coord) {
         return new Promise((resolve,reject)=>{
             con.beginTransaction(err => {
@@ -20,8 +21,8 @@ class npcTable {
                                     WHERE npc_id=${npcId};`;
 
                 let updateValuesT2 = `UPDATE designation 
-                                    SET coordinate=${coord} 
-                                    WHERE npc_id=${npcId};`;
+                                      SET coordinate=${coord} 
+                                      WHERE npc_id=${npcId};`;
 
                 // Transaction 1 for NPC table
                 con.query(updateValuesT1, (err1, result1, fields1) => {
@@ -48,29 +49,30 @@ class npcTable {
                                 reject("failed"); // Rollback and reject with error for transaction 2
                             });
                             return;
-                    }
-                    // -----------------------------------------------------------
-                    
-                    console.log("Success T2 Update");
-
-                    // -------------Commiting if no Failed Transactions-----------
-                    con.commit(err => {
-                        if (err) {
-                            con.rollback(() => {
-                                console.log("\n\nRoll Back..");
-                                reject("failed"); // Rollback and reject if commit fails
-                            });
-                        } else {
-                            resolve("success"); // Resolve with "success" status once committed
                         }
+                        // -----------------------------------------------------------
+                    
+                        console.log("Success T2 Update");
+
+                        // -------------Commiting if no Failed Transactions-----------
+                        con.commit(err => {
+                            if (err) {
+                                con.rollback(() => {
+                                    console.log("\n\nRoll Back..");
+                                    reject("failed"); // Rollback and reject if commit fails
+                                });
+                            } else {
+                                resolve("success"); // Resolve with "success" status once committed
+                            }
+                        });
+                        // ------------------------------------------------------------
                     });
-                    // ------------------------------------------------------------
                 });
-            });
-    });     
+            });     
         });
     }
 
+    // --------------- Search ----------------
     static findAll(option = "", search = "", limitCount = 0) {
         return new Promise((resolve, reject) => {
             let queryNpc = `SELECT npc_id,npc_name,npc_description,npc_image,coordinate 
@@ -105,6 +107,7 @@ class npcTable {
         });
     }
 
+    // ---------- Search Single NPC ----------
     static findSingle(npcid) {
         return new Promise((resolve,reject)=>{
             let npcDesigQuery = `SELECT npc_id,npc_name,npc_description,npc_image,coordinate 
@@ -124,6 +127,168 @@ class npcTable {
         
     }
 
+    // --------- Retrieve Max NpcID ---------
+    static checkHighestNpcId() {
+        return new Promise((resolve,reject)=>{
+            const checkHighestNpcId = `SELECT MAX(npc_id) as cur
+                                                FROM npcs;`;
+                                            
+            con.query(checkHighestNpcId, (err,result,field)=>{
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(parseInt(result[0].cur));
+
+                }
+            });           
+        }); 
+    }
+
+    // -------- Retrieve Max DesigID --------
+    static checkHighestDesigId() {
+        return new Promise((resolve,reject)=>{
+            const checkHighestDesigId = `SELECT MAX(desig_id) as cur
+                                         FROM designation;`;
+                                            
+            con.query(checkHighestDesigId, (err,result,field)=>{
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(parseInt(result[0].cur));
+                }
+            });           
+        });
+
+      
+    }
+
+    // -------- Add Npc Transaction ---------
+    static addNpc(npcId,npcName,npcDes,npcImg,coord,desigId) {
+        return new Promise((resolve,reject)=>{
+            con.beginTransaction(err => {
+                if (err) {
+                    reject(err); 
+                    return;
+                }
+        
+                let npcDesigQueryT1 = `INSERT INTO npcs(npc_id,npc_name,npc_description,npc_image)
+                                       VALUES (${npcId},"${npcName}","${npcDes}","${npcImg}");`;
+                let npcDesigQueryT2 = `INSERT INTO designation(desig_id,coordinate,quest_id,room_id,npc_id)
+                                       VALUES (${desigId},${coord},null,null,${npcId});`;  
+
+                // Transaction 1 for NPC table
+                con.query(npcDesigQueryT1, (err1, result1, fields1) => {
+
+                    // ------------------JUST FOR ERROR HANDLING----------------
+                    if (err1) { 
+                        console.log(err1)
+                        console.log("Failed T1 Update");
+                        con.rollback(() => {
+                            reject("failed"); // Rollback and reject with error for transaction 1
+                        });
+                        return;
+                    }
+                    // ---------------------------------------------------------
+
+                    console.log("Success T1");
+
+
+                    // Transaction 2 for Designation Table
+                    con.query(npcDesigQueryT2, (err2, result2, fields2) => {
+                        if (err2) {
+                            console.log("Failed T2 Update");
+                            con.rollback(() => {
+                                console.log(err2)
+                                console.log("\n\nRoll Back..");
+                                reject("failed"); // Rollback and reject with error for transaction 2
+                            });
+                            return;
+                        }
+                        // -----------------------------------------------------------
+                    
+                        console.log("Success T2 Update");
+
+                        // -------------Commiting if no Failed Transactions-----------
+                        con.commit(err => {
+                            if (err) {
+                                con.rollback(() => {
+                                    console.log("\n\nRoll Back..");
+                                    reject("failed"); // Rollback and reject if commit fails
+                                });
+                            } else {
+                                resolve("success"); // Resolve with "success" status once committed
+                            }
+                        });
+                        // ------------------------------------------------------------
+                    });
+                    
+                });
+            });     
+        });
+    }
+
+    // ------- Delete Npc Transaction -------
+    static deleteNpc(npcId) {
+        return new Promise((resolve,reject)=>{
+            con.beginTransaction(err => {
+                if (err) {
+                    reject(err); 
+                    return;
+                }
+                
+                let npcDesigDeleteT1 = `DELETE FROM designation WHERE npc_id = ${npcId};`;
+                let npcDesigDeleteT2 = `DELETE FROM npcs WHERE npc_id = ${npcId};`;  
+
+                // Transaction 1 for NPC table
+                con.query(npcDesigDeleteT1, (err1, result1, fields1) => {
+
+                    // ------------------JUST FOR ERROR HANDLING----------------
+                    if (err1) { 
+                        console.log(err1)
+                        console.log("Failed T1 Delete");
+                        con.rollback(() => {
+                            reject("failed"); // Rollback and reject with error for transaction 1
+                        });
+                        return;
+                    }
+                    // ---------------------------------------------------------
+
+                    console.log("Success T1");
+
+
+                    // Transaction 2 for Designation Table
+                    con.query(npcDesigDeleteT2, (err2, result2, fields2) => {
+                        if (err2) {
+                            console.log("Failed T2 Update");
+                            con.rollback(() => {
+                                console.log(err2)
+                                console.log("\n\nRoll Back..");
+                                reject("failed"); // Rollback and reject with error for transaction 2
+                            });
+                            return;
+                        }
+                        // -----------------------------------------------------------
+                    
+                        console.log("Success T2 Delete");
+
+                        // -------------Commiting if no Failed Transactions-----------
+                        con.commit(err => {
+                            if (err) {
+                                con.rollback(() => {
+                                    console.log("\n\nRoll Back..");
+                                    reject("failed"); // Rollback and reject if commit fails
+                                });
+                            } else {
+                                resolve("success"); // Resolve with "success" status once committed
+                            }
+                        });
+                        // ------------------------------------------------------------
+                    });
+                    
+                });
+            });
+        })
+    }
 }
  
 module.exports = npcTable;
