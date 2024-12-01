0fc79f2319e33d3578cb1e806a16955f90a13e0c
jrazec <johnrazeagno@gmail.com>
2024-12-01 13:37:23 +0800

    port

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;  // Use Vercel's dynamic port, fallback to 5000 locally.
+
 
 const userRoute = require("./server/routes/user");
 const adminRoute = require("./server/routes/admin");
